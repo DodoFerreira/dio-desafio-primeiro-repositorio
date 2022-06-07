da4be1a372b0eb196a9a9cc679b3506a9937a822
DodoFerreira <deivid.pereira@live.com>
2022-06-07 15:00:09 -0300

    Inclusão das anotações dos cursos de Git/GitHub e Lógica de Programação. Inclusão de outros materiais estudados - Introdução a Java - Anotações e exercícios no Visualg

---
+++ Estudos/IntelliJ/src/OlaMundo.java
@@ -0,0 +1,18 @@
+public class OlaMundo {
+    /**
+     * Documentação
+     */
+    // psvm método de ponto de entrada
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Olá, Mundo!");
+        System.out.println("Autoformatação ctrl+alt+L");
+        /*comentário
+        de
+        várias linhas
+
+         */
+
+    }
+
+}
---
+++ Estudos/Java/JavaCompilacao.java
@@ -0,0 +1,5 @@
+public class JavaCompilacao {
+	public static void main(String[] args) {
+		System.out.println("Sei lá qualquer coisa da o cu e pá");
+	}
+}
